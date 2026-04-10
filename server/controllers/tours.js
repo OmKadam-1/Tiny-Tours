e487776e6ddcb48945bbfd2152fb619836857235
OmKadam-1 <kom110798@gmail.com>
2026-04-10 11:50:04 +0530

    tours controller completed

--- server/controllers/tours.js
+++ server/controllers/tours.js
@@ -14,3 +14,76 @@ const getTours = async (req, res) => {
         data: tours,
     });
 };
+
+const postTours = async (req, res) => {
+    const { title, description, cities, startDate, endDate, photos } =
+        req.body;
+
+    const newTour = new Tour({
+        title,
+        description,
+        cities,
+        startDate,
+        endDate,
+        photos,
+        user: req.user.id,
+    });
+
+    try {
+        const savedTour = await newTour.save();
+
+        return res.json({
+            success: true,
+            message: "Tour created successfully",
+            data: savedTour,
+        });
+    } catch (error) {
+        return res.json({
+            success: false,
+            message: `Tour creation failed: ${error.message}`,
+            data: null,
+        });
+    }
+};
+
+const putTours = async (req, res) => {
+    const user = req.user;
+    const userId = user.id;
+    const { id } = req.params;
+
+    const tour = await Tour.findById(id);
+
+    if (!tour) {
+        return res.json({
+            success: false,
+            message: "Tour not found",
+            data: null,
+        });
+    }
+
+    if (tour.user.toString() !== userId) {
+        return res.json({
+            success: false,
+            message: "Unauthorized to update this tour",
+            data: null,
+        });
+    }
+
+    const { title, description, cities, startDate, endDate, photos } =
+        req.body;
+
+    await Tour.updateone(
+        { _id: id },
+        { title, description, cities, startDate, endDate, photos }
+    );
+
+    const updatedTour = await Tour.findById(id);
+
+    return res.json({
+        success: true,
+        message: "Tour update authorized",
+        data: updatedTour,
+    });
+};
+
+export { getTours, postTours, putTours };
