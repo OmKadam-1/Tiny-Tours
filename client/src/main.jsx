f53d2cb9406fdc99f1f918c219c0c1496cff8128
OmKadam-1 <kom110798@gmail.com>
2026-04-10 17:55:03 +0530

    routes corrected

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,6 +4,11 @@ import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
 import Contact from "./views/Contact.jsx";
 import Dashboard from "./views/Dashboard.jsx";
+import EditTour from "./views/EditTour.jsx";
+import Login from "./views/Login.jsx";
+import Signup from "./views/Signup.jsx";
+import Tours from "./views/Tours.jsx";
+
 
 const root = createRoot(document.getElementById("root"));
 
@@ -11,6 +16,13 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Dashboard />} />
+      <Route path="/tours" element={<Tours />} />
+      <Route path="/tours/new" element={<EditTour />} />
+      <Route path="/tours/edit/:id" element={<EditTour />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      
     
     </Routes>
   </BrowserRouter>,
