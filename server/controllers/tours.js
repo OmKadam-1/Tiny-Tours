286aaf703f8345476ed752c8949ab0bb04e0fb9b
OmKadam-1 <kom110798@gmail.com>
2026-04-10 11:49:04 +0530

    get tour controller

--- server/controllers/tours.js
+++ server/controllers/tours.js
@@ -1,3 +1,16 @@
 import Tour from "../models/Tour";
 import dotenv from "dotenv";
 dotenv.config();
+
+const getTours = async (req, res) => {
+    const tours = await Tour.find({ user: req.user.id }).populate(
+        "user",
+        "-password"
+    );
+
+    return res.json({
+        success: true,
+        message: "Fetched tours successfully",
+        data: tours,
+    });
+};
