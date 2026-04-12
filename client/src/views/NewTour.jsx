85a7684b0ec19b9839fecd5812f504fd50f586c5
OmKadam-1 <kom110798@gmail.com>
2026-04-12 09:33:48 +0530

    tour loading problem soln

--- client/src/views/NewTour.jsx
+++ client/src/views/NewTour.jsx
@@ -33,7 +33,7 @@ function NewTour() {
   const authenticator = async () => {
     try {
       // Perform the request to the upload authentication endpoint.
-      const response = await fetch("http://localhost:8080/auth");
+      const response = await fetch`${import.meta.env.VITE_API_BASE_URL}/auth`;
       if (!response.ok) {
         // If the server response is not successful, extract the error text for debugging.
         const errorText = await response.text();
